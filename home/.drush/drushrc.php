9aa47be72f8450331a8232c8bf86947e9d0d06a5
Elijah Lynn <elijah@elijahlynn.net>
2014-08-06 18:24:11 -0400

    Added command to get current site alias & download git package with info.

---
+++ home/.drush/drushrc.php
@@ -0,0 +1,4 @@
+<?php
+
+$options['shell-aliases']['site-get'] = '@none php-eval "return drush_sitealias_site_get();"';
+$options['shell-aliases']['pm-clone'] = 'pm-download --gitusername=YOURUSERNAME --package-handler=git_drupalorg';
