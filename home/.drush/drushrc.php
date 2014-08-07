298963e68dee20fe57d35eaef49cd398203d473a
Elijah Lynn <elijah@elijahlynn.net>
2014-08-06 22:44:46 -0400

    Added default password to drushrc for drush installs.

--- home/.drush/drushrc.php
+++ home/.drush/drushrc.php
@@ -4,5 +4,8 @@
 $options['drush_usage_log'] = TRUE;
 $options['drush_usage_send'] = TRUE;
 
+# Default password for drush installs.
+$options['account-pass'] = 'pa55word';
+
 $options['shell-aliases']['site-get'] = '@none php-eval "return drush_sitealias_site_get();"';
 $options['shell-aliases']['pm-clone'] = 'pm-download --gitusername=ElijahLynn --package-handler=git_drupalorg';
