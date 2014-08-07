51983b75ab4d00ff471eb0416f496afcef79cae8
Elijah Lynn <elijah@elijahlynn.net>
2014-08-06 22:41:37 -0400

    Added correct username to pm-clone.

--- home/.drush/drushrc.php
+++ home/.drush/drushrc.php
@@ -1,4 +1,4 @@
 <?php
 
 $options['shell-aliases']['site-get'] = '@none php-eval "return drush_sitealias_site_get();"';
-$options['shell-aliases']['pm-clone'] = 'pm-download --gitusername=YOURUSERNAME --package-handler=git_drupalorg';
+$options['shell-aliases']['pm-clone'] = 'pm-download --gitusername=ElijahLynn --package-handler=git_drupalorg';
