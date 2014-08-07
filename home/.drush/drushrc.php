6e8d346ed0aafec133f7912608d2bf04f8526ccd
Elijah Lynn <elijah@elijahlynn.net>
2014-08-06 22:43:31 -0400

    Added usage data sending to drushrc.

--- home/.drush/drushrc.php
+++ home/.drush/drushrc.php
@@ -1,4 +1,8 @@
 <?php
 
+# Added per README.txt to help send usage data.
+$options['drush_usage_log'] = TRUE;
+$options['drush_usage_send'] = TRUE;
+
 $options['shell-aliases']['site-get'] = '@none php-eval "return drush_sitealias_site_get();"';
 $options['shell-aliases']['pm-clone'] = 'pm-download --gitusername=ElijahLynn --package-handler=git_drupalorg';
