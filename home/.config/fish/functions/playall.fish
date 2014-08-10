2c00d92678451c3b725cb0c2a492c484b681cc29
Elijah Lynn <elijah@elijahlynn.net>
2014-08-09 20:46:01 -0400

    Added 'playall' fish command.

---
+++ home/.config/fish/functions/playall.fish
@@ -0,0 +1,9 @@
+function playall --description 'Plays all audio files in a directory, accepts the file extension as first argument.'
+	for i in *.$argv[1]
+		set start (date +%s);
+		while test ( math $start + 1 ) -ge (date +%s)
+			echo $i
+			paplay $i 
+		end
+	end
+end
