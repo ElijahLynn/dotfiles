449596bbf6c8142cb502c867da3e26232dbfceb1
Elijah Lynn <elijah@elijahlynn.net>
2014-08-13 17:28:51 -0400

    Add Drush default location for temp directory.

--- home/.drush/drushrc.php
+++ home/.drush/drushrc.php
@@ -9,3 +9,5 @@ $options['account-pass'] = 'pa55word';
 
 $options['shell-aliases']['site-get'] = '@none php-eval "return drush_sitealias_site_get();"';
 $options['shell-aliases']['pm-clone'] = 'pm-download --gitusername=ElijahLynn --package-handler=git_drupalorg';
+
+$options['file_temporary_path'] = '/tmp';
