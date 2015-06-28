51d021b2523224aac0aaa66047083cabc4904be3
Elijah Lynn <elijah@elijahlynn.net>
2015-06-28 13:31:02 -0400

    Update dotfiles

--- home/.oh-my-fish/custom/homeshick_backup.fish
+++ home/.oh-my-fish/custom/homeshick_backup.fish
@@ -1,19 +1,22 @@
 #! /usr/bin/fish
 # Runs every minute via crontab.
 
-# Export Fish abbreviations and commit them.
 cd $HOME/.homesick/repos/dotfiles
-abbr --show | sort > fish_abbreviation_backup
-if not git diff --exit-code fish_abbreviation_backup
-    git add fish_abbreviation_backup
-    git commit --message "Update Fish abbreviations"
-end
 
 # Commit any dotfiles changes.
 if not git diff --exit-code; git diff --cached --exit-code; git ls-files --other --exclude-standard --directory
+    # Export Fish abbreviations and commit them.
+    abbr --show | sort > fish_abbreviation_backup
+    if not git diff --exit-code fish_abbreviation_backup
+        git add fish_abbreviation_backup
+        git commit --message "Update Fish abbreviations"
+    end
+
+    # Update dotfiles.
     git add --all
     git commit --message "Update dotfiles"
-    # Push & Notify
+
+    # Push & Notify.
     git push
     notify-send "Dotfiles updated and pushed to Github"
 end
