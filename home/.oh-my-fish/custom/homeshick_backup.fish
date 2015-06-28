96febf4b3e26925a6e18b9d4c721169e2b26b357
Elijah Lynn <elijah@elijahlynn.net>
2015-06-28 16:24:02 -0400

    Update dotfiles

--- home/.oh-my-fish/custom/homeshick_backup.fish
+++ home/.oh-my-fish/custom/homeshick_backup.fish
@@ -1,27 +1,31 @@
 #! /usr/bin/fish
 # Runs every 5 minutes via crontabz.
+
 source $HOME/.keychain/(hostname)-fish
 notify-send --expire-time=1000 $SSH_AGENT_PID
 
 cd $HOME/.homesick/repos/dotfiles
 
 # Commit any dotfiles changes.
-if not git diff --exit-code; not git diff --cached --exit-code; git ls-files --other --exclude-standard --directory; abbr --show | sort > fish_abbreviation_backup
-    # Export Fish abbreviations and commit them.
-    if not git diff --exit-code fish_abbreviation_backup
-        git add fish_abbreviation_backup
-        git commit --message "Update Fish abbreviations"
-    end
+if not git diff --exit-code;
+   not git diff --cached --exit-code;
+   git ls-files --other --exclude-standard --directory;
+   abbr --show | sort > fish_abbreviation_backup;
+        # Export Fish abbreviations and commit them.
+        if not git diff --exit-code fish_abbreviation_backup
+            git add fish_abbreviation_backup
+            git commit --message "Update Fish abbreviations"
+        end
 
-    # Update dotfiles.
-    git add --all
-    git commit --message "Update dotfiles"
+        # Update dotfiles.
+        git add --all
+        git commit --message "Update dotfiles"
 
-    # Push & Notify.
-    git push
-    if test (git rev-parse --verify master) = (git rev-parse --verify origin/master)
-        notify-send --expire-time=1000 "Dotfiles updated and pushed to Github"
-    else
-        notify-send --expire-time=1000 "There was a problem pushing your dotfiles to Github"
-    end
+        # Push & Notify.
+        git push
+        if test (git rev-parse --verify master) = (git rev-parse --verify origin/master)
+            notify-send --expire-time=1000 "Dotfiles updated and pushed to Github"
+        else
+            notify-send --expire-time=1000 "There was a problem pushing your dotfiles to Github"
+        end
 end
