a6efd4cb8bf309b4bbbb6230a41d4793a99f4d49
Elijah Lynn <elijah@elijahlynn.net>
2015-06-28 14:03:01 -0400

    Update dotfiles

--- home/.oh-my-fish/custom/homeshick_backup.fish
+++ home/.oh-my-fish/custom/homeshick_backup.fish
@@ -25,3 +25,25 @@ if not git diff --exit-code; not git diff --cached --exit-code; git ls-files --o
     end
 end
 
+
+# Export Fish abbreviations and commit them.
+if false
+
+    abbr --show | sort > fish_abbreviation_backup
+    if not git diff --exit-code fish_abbreviation_backup
+        git add fish_abbreviation_backup
+        git commit --message "Update Fish abbreviations"
+    end
+
+    # Update dotfiles.
+    git add --all
+    git commit --message "Update dotfiles"
+
+    # Push & Notify.
+    git push
+    if test (git rev-parse --verify master) = (git rev-parse --verify origin/master)
+        notify-send "Dotfiles updated and pushed to Github"
+    else
+        notify-send "There was a problem pushing your dotfiles to Github"
+    end
+end
