fd6a9f2ac1ea0615bfe0a7b66d5e1d5fea4385e2
Elijah Lynn <elijah@elijahlynn.net>
2015-06-28 15:59:02 -0400

    Update dotfiles

--- home/.oh-my-fish/custom/homeshick_backup.fish
+++ home/.oh-my-fish/custom/homeshick_backup.fish
@@ -3,6 +3,8 @@
 notify-send $SSH_AUTH_SOCK
 notify-send $SSH_AGENT_PID
 source $HOME/.keychain/(hostname)-fish
+notify-send $SSH_AUTH_SOCK
+notify-send $SSH_AGENT_PID
 
 cd $HOME/.homesick/repos/dotfiles
 
