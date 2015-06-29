d17a1e1abe160f7f1427ac3d5345f5b09acd2d7e
Elijah Lynn <elijah@elijahlynn.net>
2015-06-28 20:54:02 -0400

    Update dotfiles

--- home/.config/fish/functions/rm.fish
+++ home/.config/fish/functions/rm.fish
@@ -1,5 +1,6 @@
 function rm
-	if not status --is-login; and status --is-interactive
+	breakpoint
+if not status --is-login; and status --is-interactive
 	    echo "This is not the command you are looking for, use trash-put instead."; false
         end
 end
