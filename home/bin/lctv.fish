a8f1087b04153baaad05029ebe00c06ff58fcea3
Elijah Lynn <elijah@elijahlynn.net>
2015-07-06 12:55:02 -0400

    Update Fish abbreviations

---
+++ home/bin/lctv.fish
@@ -0,0 +1,11 @@
+#! /usr/bin/fish
+
+
+
+if test (pgrep "obs")
+        set viewer_count (curl  https://www.livecoding.tv/livestreams/elijahlynn/stats.json | jq .views_live)
+	echo ' - '  $viewer_count
+
+else
+	echo " -"
+end
