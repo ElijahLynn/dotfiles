95e4f38a54d34fd96f4a1c906b82647e47a1a61b
Elijah Lynn <elijah@elijahlynn.net>
2016-01-07 23:26:01 -0500

    Update functions

--- home/.config/fish/functions/rm.fish
+++ home/.config/fish/functions/rm.fish
@@ -1,6 +1,5 @@
 function rm
-	echo (status --is-command-substitution)
-    if status --is-login; and status --is-interactive
+	if status --is-login; and status --is-interactive
 	    echo "This is not the command you are looking for, use trash-put instead.";
         end
 end
