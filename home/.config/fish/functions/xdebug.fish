3ed5609e3c845d6fc79c4b358abf7e0b7d6c7380
Elijah Lynn <elijah@elijahlynn.net>
2016-02-01 08:37:02 -0500

    Update functions

--- home/.config/fish/functions/xdebug.fish
+++ home/.config/fish/functions/xdebug.fish
@@ -1,10 +1,10 @@
 function xdebug
 	switch $argv[1]
-		case 'on'
-			set -xU XDEBUG_CONFIG 'idekey=phpstorm'
-			echo Xdebug activated
-		case 'off'
-			set -e XDEBUG_CONFIG
-			echo Xdebug deactivated
-	end
+	case 'on'
+            set -xU XDEBUG_CONFIG 'idekey=phpstorm'
+            echo Xdebug activated
+	case 'off'
+            set -e XDEBUG_CONFIG
+	    echo Xdebug deactivated
+    end
 end
