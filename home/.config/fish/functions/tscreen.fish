4b0dad57e514e4abab56a2aa02b1637f8ba4cb82
Elijah Lynn <elijah@elijahlynn.net>
2016-02-01 08:27:02 -0500

    Update functions

--- home/.config/fish/functions/tscreen.fish
+++ home/.config/fish/functions/tscreen.fish
@@ -1,11 +1,11 @@
 function tscreen
-	set ID (xinput list | grep Wacom | awk -F= '{ print $2 }' | cut -c1-2)
-switch $argv[1]
-case 'on'
-xinput enable $ID
-echo 'Touchscreen enabled'
-case 'off'
-xinput disable $ID
-echo 'Touchscreen disabled'
-end
+    set ID (xinput list | grep Wacom | awk -F= '{ print $2 }' | cut -c1-2)
+    switch $argv[1]
+        case 'on'
+            xinput enable $ID
+            echo 'Touchscreen enabled'
+        case 'off'
+            xinput disable $ID
+            echo 'Touchscreen disabled'
+    end
 end
