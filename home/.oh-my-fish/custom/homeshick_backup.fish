f2cecdf54d57d03f8e22cc44cae8a4c94d958f78
Elijah Lynn <elijah@elijahlynn.net>
2016-04-22 14:42:02 -0400

    Update functions

--- home/.oh-my-fish/custom/homeshick_backup.fish
+++ home/.oh-my-fish/custom/homeshick_backup.fish
@@ -18,7 +18,6 @@ cd $HOME/.homesick/repos/dotfiles
 # keychain support, so crontab doesn't ask for a key passphrase every git push.
 # @see http://superuser.com/a/933903/30982
 source $HOME/.keychain/(hostname)-fish
-notify-send '40'
 notify-send (date)
 
 
