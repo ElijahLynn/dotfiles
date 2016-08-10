9f14c805240d5d98c5fa3251257652a917492bb4
Elijah Lynn <elijah@elijahlynn.net>
2016-08-10 14:26:58 -0400

    Fix alert to a.fish symlink issue.

--- home/.config/fish/functions/alert.fish
+++ home/.config/fish/functions/alert.fish
@@ -1,1 +1,6 @@
-/home/elijah/.homesick/repos/dotfiles/home/.config/fish/functions/a.fish
+function alert
+	if contains -- --sms $argv[1]
+	echo 'Job is done with a status of ' $status ' @' (date)| twilio-sms 903-354-5247 > /dev/null 
+        end
+        paplay ~/.config/fish/functions/TaDa.ogg
+end
