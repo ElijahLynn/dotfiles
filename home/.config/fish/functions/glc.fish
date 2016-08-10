f3e53a9641bb206ecb7f128a137e28b1f825f7fe
Elijah Lynn <elijah@elijahlynn.net>
2016-08-10 14:14:11 -0400

    Improve fish function descriptions.

--- home/.config/fish/functions/glc.fish
+++ home/.config/fish/functions/glc.fish
@@ -1,3 +1,3 @@
-function glc
+function glc --description 'Git log the last commit into the clipboard'
 	git log --pretty=format:%s -1 | xsel -i --clipboard
 end
--- home/.config/fish/functions/playall.fish
+++ home/.config/fish/functions/playall.fish
@@ -1,4 +1,4 @@
-function playall --description 'Plays all audio files in a directory, accepts the file extension as first argument.'
+function playall --description 'Plays all audio files in a directory, accepts the file extension as first argument. Good for testing soundbytes, system sounds.'
 	for i in *.$argv[1]
 		set start (date +%s);
 		while test ( math $start + 1 ) -ge (date +%s)
