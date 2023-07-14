d2961c4b89249572fade948aeeb89c2d0218cf4a
Elijah Lynn <elijah.lynn@swirldslabs.com>
2023-07-14 16:20:19 -0700

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,7 +7,7 @@ source ~/.iterm2_shell_integration.fish
 ## asdf support
 ## https://asdf-vm.com/guide/getting-started.html
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
- 
+
 set --global fish_greeting ""
 
 function just_the_time --description 'Shows just the time in 24-hour format'
@@ -16,10 +16,13 @@ end
 
 function fish_prompt
     echo ""
-    echo "----------------------------------------------"
+    echo ----------------------------------------------
+    echo -n (just_the_time)
+    echo " RC: $status"
+    echo ----------------------------------------------
+ 
     set -g __fish_git_prompt_show_informative_status yes
     set -g __fish_git_prompt_showuntrackedfiles yes
-    echo -n (just_the_time)
     echo -e (set_color $fish_color_cwd) (prompt_pwd)(set_color brown)(fish_git_prompt)
     # set --erase --global branch
     echo (iterm2_prompt_mark) ""
