514ce9797e59d8565266be9ad945fbde2a753d98
Elijah Lynn <elijah@elijahlynn.net>
2024-11-27 16:50:39 -0800

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -13,6 +13,7 @@ fish_add_path (find ~/.sdkman/candidates/*/current/bin -maxdepth 0)
 ## https://stackoverflow.com/a/77054092/292408
 set --global --export PATH $HOMEBREW_PREFIX/opt/grep/libexec/gnubin $PATH
 
+# For non-warp shells
 set --global fish_greeting ""
 
 # Created by `pipx` on 2023-11-09 23:11:55
@@ -22,4 +23,3 @@ source ~/.config/fish/completions/kubectl.fish
 # Add
 # https://direnv.net/docs/hook.html
 direnv hook fish | source
-
